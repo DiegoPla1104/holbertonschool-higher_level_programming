369fe86984b9da8a3b65495ff673cf5844d14d87
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-19 13:30:14 +0000

    fix: variable error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
-if lastdigit == 0:
+if last == 0:
     print(f"Last digit of {number} is {last} and is 0")
-elif lastdigit > 5:
+elif last > 5:
     print(f"Last digit of {number} is {last} and is grater than 5")
 else:
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
