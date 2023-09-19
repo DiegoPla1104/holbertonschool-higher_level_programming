5f5aca2367ea6c94d60659f7f131bea9e68cef5c
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-19 14:04:41 +0000

    fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,16 +5,16 @@ if number < 0:
     las = abs(number) % 10 * -1
 else:
     las = abs(number) % 10
-    
+
 if las == 0:
     print(f"Last digit of {number} is {las} and is 0")
 elif las > 5:
     if number < 0:
-        print(f"Last digit of {number} is {-las} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {las} and is less than 6 and not 0")
     else:
         print(f"Last digit of {number} is {las} and is grater than 5")
 elif las < 6 and not 0:
     if number < 0:
-        print(f"Last digit of {number} is {-las} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {las} and is less than 6 and not 0")
     else:
         print(f"Last digit of {number} is {las} and is less than 6 and not 0")
