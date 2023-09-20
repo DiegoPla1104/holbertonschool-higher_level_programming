ba43072bef841fafa0134393386e18d685cb2544
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-20 15:14:00 +0000

    fix: missing characters

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1, 100):
+    for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
             x = "FizzBuzz"
         elif x % 3 == 0:
@@ -10,4 +10,3 @@ def fizzbuzz():
         else:
             x = x
         print("{} ".format(x), end='')
-    print("Buzz")
