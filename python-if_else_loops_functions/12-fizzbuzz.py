99f26fc0997cb7fcce702a4f11cd50de516f4299
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-20 15:11:02 +0000

    feat: task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 100):
+        if x % 3 == 0 and x % 5 == 0:
+            x = "FizzBuzz"
+        elif x % 3 == 0:
+            x = "Fizz"
+        elif x % 5 == 0:
+            x = "Buzz"
+        else:
+            x = x
+        print("{} ".format(x), end='')
+    print("Buzz")
