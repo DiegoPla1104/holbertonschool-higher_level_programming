10752a2a8596c810b3696e9662ab4630ed692b48
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-24 22:48:14 +0000

    fix: missing space

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,4 +6,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         aa = (tuple_a[0], 0)
     if len(tuple_b) == 1:
         bb = (tuple_b[0], 0)
-    return(aa[0] + bb[0], aa[1] + bb[1])
+    return (aa[0] + bb[0], aa[1] + bb[1])
