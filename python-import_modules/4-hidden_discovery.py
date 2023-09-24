d589040e800ed6bfa95ba5057fdc713e09cba857
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-24 22:52:36 +0000

    fix: ?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for listt in dir(hidden_4):
-        if (listt[0] != '_' and lista[1] != '_'):
+        if (listt[0] != '_' and listt[1] != '_'):
             print(listt)
