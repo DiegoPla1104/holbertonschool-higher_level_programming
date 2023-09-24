0414080a41914668af4fc1421b7134b6fc25e6c1
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-24 22:51:59 +0000

    fix: printing error

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for list in dir(hidden_4):
-        if (list[0] != '_' and lista[1] != '_'):
-            print(lista)
+    for listt in dir(hidden_4):
+        if (listt[0] != '_' and lista[1] != '_'):
+            print(listt)
