30c4bc4c68c1dccc6723d4f59323418b60feb4a8
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-26 13:54:34 +0000

    fix: task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    result = str(sorted(a_dictionary, key=lambda key: a_dictionary[key]))
-    print(result)
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
