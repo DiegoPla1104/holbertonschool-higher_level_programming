96b6c8b85a65d3ae053f46ecafbdce3c43493f34
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-27 23:56:16 +0000

    fix: pycodestyle error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     nprinted = 0
     for iter in range(x):
-        try:  
+        try:
             print("{:d}".format(my_list[iter]), end="")
             nprinted += 1
         except (ValueError, TypeError):
