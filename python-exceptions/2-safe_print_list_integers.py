ffc87491dc4be3ec56ba2db91572627f8da40b6c
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-27 23:53:13 +0000

    fix: IterError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     nprinted = 0
     for iter in range(x):
         try:
-            print("{:d}".format(my_list[iter]), end="")
-            nprinted += 1
-        except (ValueError, TypeError, IndexError):
+            if iter < len(my_list) - 1:    
+                print("{:d}".format(my_list[iter]), end="")
+                nprinted += 1
+        except (ValueError, TypeError):
             pass
     print()
     return nprinted
