f695fc68e5db1d22b1f83626dd445557619eedcb
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-27 23:55:02 +0000

    fix: functionality fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     nprinted = 0
     for iter in range(x):
-        try:
-            if iter < len(my_list) - 1:    
-                print("{:d}".format(my_list[iter]), end="")
-                nprinted += 1
+        try:  
+            print("{:d}".format(my_list[iter]), end="")
+            nprinted += 1
         except (ValueError, TypeError):
             pass
     print()
