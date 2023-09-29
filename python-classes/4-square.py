ad36ded9638ad1bc148306fec65fb1eba2fd9050
DiegoPla1104 <diegopla1104@gmail.com>
2023-09-29 12:47:48 +0000

    fix: pycodestyle error

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
     @property
     def size(self):
         """Property setter for square"""
-        return(self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -26,4 +26,4 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        return (self.__size * self.__size)
+        return self.__size * self.__size
