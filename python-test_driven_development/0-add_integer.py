12c8f2109ec71f3710098a033d74a611730e8ea1
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-01 14:46:29 +0000

    feat: task 0 and 1

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    This Module is used to add two numbers
+"""
+
+
+def add_integer(a, b=98):
+    """
+        The two variables that are passed
+        to this function are a and b.
+        First I'm gonna check that they are an int
+        or a float so that the addition can be done.
+        If they aren't a TypeError will be used.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    This module divies the ints inide a matrix
+    by a specified number
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        This is said function.
+        It cheacks that the matrix holds only integers
+        If not raises a type error
+        Then checks the div if its an int or a float and if it's 0
+        Afterwards checks if the rows have the same length
+        And finally creates a new list and appends the result of the division
+    """
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    l0 = []
+    for a in matrix:
+        if type(a) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
+        if len(a) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have \
+the same size")
+
+        l1 = []
+        for b in a:
+            if type(b) is not int and type(b) is not float:
+                raise TypeError("matrix mut be a matrix (list of lists) of \
+integers/floats")
+
+            l1.append(round(b / div, 2))
+        l0.append(l1)
+    return l0
