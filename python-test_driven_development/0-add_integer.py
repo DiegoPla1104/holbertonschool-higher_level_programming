119016c086acc8d1d9bc52afb64c7404041036cb
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-01 17:57:29 +0000

    fix: ?

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
     This Module is used to add two numbers
-
-
 """
 
 
 def add_integer(a, b=98):
     """
-        The two variables that are passed to this function are a and b.
+        The two variables that are passed
+        to this function are a and b.
+        First I'm gonna check that they are an int
+        or a float so that the addition can be done.
+        If they aren't a TypeError will be used.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
@@ -16,4 +18,4 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
-    return a + b
+    return (a + b)
