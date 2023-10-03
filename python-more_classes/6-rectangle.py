014f058366febe540d4590d6abda5ba057a4bd09
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-03 00:08:13 +0000

    fix: pycodestyle error

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
         Creating the attribute for sai object
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
