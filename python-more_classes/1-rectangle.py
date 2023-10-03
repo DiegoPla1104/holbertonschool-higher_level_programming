97fc498d10ed78e45ec520d32891a52ecd9a3fa5
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-03 00:01:26 +0000

    fix: missing conditions

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,6 +12,16 @@ class Rectangle:
         self.__height = height
         self.__width = width
 
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+
     @property
     def height(self):
         """Property setter for square"""
