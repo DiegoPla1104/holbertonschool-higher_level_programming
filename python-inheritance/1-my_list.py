381a063109c480b02b3ee0e9e435731a91ed0807
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-06 00:04:27 +0000

    feat: task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module to print a sorted list
+"""
+
+
+class MyList(list):
+    """ 
+        Class named MyList
+    """
+
+    def print_sorted(self):
+        """
+            Prints the list but sorted (ascending sort)
+        """
+        print(sorted(self))
