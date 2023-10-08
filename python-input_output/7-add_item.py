ca3505f5c3c69fc298046eb11c277be0ee9aaae5
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-08 18:25:06 +0000

    fix: ?

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -14,6 +14,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+open("add_item.json", "a")
 try:
     f = load_from_json_file("add_item.json") + argv[1:]
     save_to_json_file(f, "add_item.json")
