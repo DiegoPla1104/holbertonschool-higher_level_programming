adb0755fe864e73f81205acab5f07b44533ac04f
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-10 13:23:27 +0000

    feat: task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,8 +28,8 @@ class Rectangle(Base):
         """Defining width cualifications"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
+        elif value < 1:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -42,8 +42,8 @@ class Rectangle(Base):
         """Defining height cualifications"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        elif value < 1:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
