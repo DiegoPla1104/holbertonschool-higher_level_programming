aa94a3713b1a1d4f8b37e60c6f6f895cbe5d4fd8
DiegoPla1104 <diegopla1104@gmail.com>
2023-10-15 13:51:11 +0000

    boring

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import unittest
-Base = __import__(models/base.py).Base
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 """
     Tests for the file named base.py
 """
@@ -12,3 +14,97 @@ class TestBase(unittest.TestCase):
 
     def TestIdNum(self):
         self.assertEqual(id, 12312)
+
+    def test_arg_02(self):
+        """with one object created but with argument"""
+        Base._Base__nb_objects = 0
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+        b3 = Base(0)
+        self.assertEqual(b3.id, 0)
+        b4 = Base(98)
+        self.assertEqual(b4.id, 98)
+        b5 = Base(-8)
+        self.assertEqual(b5.id, -8)
+        b6 = Base(9)
+        self.assertEqual(b6.id, 9)
+
+    def test_arg_04(self):
+        """multiple objects without argument
+        and one with one argument"""
+        Base._Base__nb_objects = 0
+        b1 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+
+    def test_save_Rectangle_empty_list(self):
+        """saving to file with none"""
+        Base._Base__nb_objects = 0
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            read = file.read()
+            my_list = Base.from_json_string(read)
+            self.assertListEqual(my_list, [])
+
+    def test_save_rectangle_only(self):
+        """ saving of json representation of objects to file
+        for Rectangle only"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 7, 2, 8)
+        Rectangle.save_to_file([r1])
+        with open("Rectangle.json", "r") as file:
+            read = file.read()
+            my_list = Base.from_json_string(read)
+            self.assertDictEqual(r1.to_dictionary(), my_list[0])
+
+    def test_save_Square(self):
+        """ savingjson representation to file"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 7, 2, 8)
+        s2 = Square(2)
+        Square.save_to_file([r1, s2])
+        with open("Square.json", "r") as file:
+            read = file.read()
+            my_list = Base.from_json_string(read)
+            self.assertDictEqual(r1.to_dictionary(), my_list[0])
+            self.assertDictEqual(s2.to_dictionary(), my_list[1])
+
+    def test_save_Square_none(self):
+        """saving to file with none"""
+        Base._Base__nb_objects = 0
+        Square.save_to_file(None)
+        with open("Square.json", "r") as file:
+            read = file.read()
+            my_list = Base.from_json_string(read)
+            self.assertListEqual(my_list, [])
+
+    def test_save_Square_only_square(self):
+        """saving to file with only Square"""
+        Base._Base__nb_objects = 0
+        s1 = Square(1)
+        Square.save_to_file([s1])
+        with open("Square.json", "r") as file:
+            read = file.read()
+            my_list = Base.from_json_string(read)
+            self.assertDictEqual(s1.to_dictionary(), my_list[0])
+
+    def test_create_Rectangle(self):
+        """Rectangle new instance"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertFalse(r1 is r2)
+        self.assertDictEqual(r1_dictionary, r2.to_dictionary())
+
+    def test_create_Square(self):
+        """creating of new Square instance"""
+        Base._Base__nb_objects = 0
+        s1 = Square(3, 5, 1)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertFalse(s1 is s2)
+        self.assertDictEqual(s1_dictionary, s2.to_dictionary())
