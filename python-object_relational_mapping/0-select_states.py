e6dc4fa6f7ff168d66dca2bf3757c30728453483
DiegoPla1104 <diegopla1104@gmail.com>
2023-11-19 23:54:33 +0000

    fix: pycodestyle identation and missplaced spaces

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    data_connector = MySQLdb.connect(host = "localhost",
-                                   port = 3306,
-                                   user = sys.argv[1],
-                                   passwd = sys.argv[2],
-                                   db = sys.argv[3])
+    data_connector = MySQLdb.connect(host="localhost",
+                                     port=3306,
+                                     user=sys.argv[1],
+                                     passwd=sys.argv[2],
+                                     db=sys.argv[3])
     cursor = data_connector.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id;")
     for row in cursor.fetchall():
