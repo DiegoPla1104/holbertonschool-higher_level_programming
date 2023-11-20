5facf20dc4ff5d2b6606e41293a10eef78171a24
DiegoPla1104 <diegopla1104@gmail.com>
2023-11-20 01:23:59 +0000

    fix: the dumbest mistake of my live

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cursor = data_connector.cursor()
     cursor.execute(f"SELECT cities.name FROM cities JOIN states ON \
         cities.state_id = states.id WHERE states.name LIKE BINARY \
-            {sys.argv[4]} ORDER BY cities.id;")
+            '{sys.argv[4]}' ORDER BY cities.id;")
     listed = []
     for row in cursor.fetchall():
         listed.append(row[0])
